c0011f118ec628f12c62fc122ddc663a58d163fa
Adolfo Chavez <adolfo.dev1295@gmail.com>
2018-02-05 23:31:46 -0600

    Se añadio un metodo con un toast

--- logutil/src/main/java/logexample/adolfodev1295/com/logutil/LogDebug.java
+++ logutil/src/main/java/logexample/adolfodev1295/com/logutil/LogDebug.java
@@ -1,6 +1,8 @@
 package logexample.adolfodev1295.com.logutil;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by user on 05/02/2018.
@@ -10,5 +12,8 @@ public class LogDebug {
     public static final String TAG = "Hola";
     public static void hola(String nombre){
         Log.d(TAG,"Hola! "+nombre);
+    }
+    public static void saludo(Context context, String nombre){
+        Toast.makeText(context, "Hola! "+nombre, Toast.LENGTH_SHORT).show();
     }
 }
